a4dec868e88c6be2155270efeacd3555837a5af6
user <user@w7>
2019-02-24 18:39:04 +0800

    asd

---
+++ shoppingcart/src/shoppingcart/sessiontest.java
@@ -0,0 +1,35 @@
+package shoppingcart;
+
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class sessiontest
+ */
+@WebServlet("/sessiontest")
+public class sessiontest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String 加入購物單 = request.getParameter("ses");
+		//String sell = request.getParameter("sell");
+		//Optional<String> sell = Optional.ofNullable(request.getParameter("sell"));
+		if(request.getParameter("sell")!=null){
+			request.getSession().removeAttribute(加入購物單);
+			response.sendRedirect("shoppingcart.jsp");
+		}else{
+			request.getSession().setAttribute(加入購物單, 加入購物單);
+			response.sendRedirect("home.jsp");
+		}
+		
+	}
+
+}
